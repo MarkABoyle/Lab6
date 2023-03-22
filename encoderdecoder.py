bd8814aae7a11f93db2adf1447b5589c4106b3e1
MarkABoyle <mark.boyle@ufl.edu>
2023-03-22 18:49:07 -0400

    implimented decode

--- encoderdecoder.py
+++ encoderdecoder.py
@@ -1,11 +1,15 @@
+# Mark Boyle
+from encoderdecoder2 import decode
+
+
 def encode(password):
     listpass = list(password)
     for i in range(0, len(listpass)):
         listpass[i] = str(int(listpass[i]) + 3)
-	if int(listpass[i]) >= 10:
-		listpass[i] = str(int(listpass[i]) - 10)
-	else:
-		continue
+        if int(listpass[i]) >= 10:
+            listpass[i] = str(int(listpass[i]) - 10)
+        else:
+            continue
     newlist = ''.join(listpass)
     return newlist
 
@@ -18,14 +22,13 @@ def main():
         print("1. Encode\n2. Decode\n3. Quit\n")
         menu_input = input("Please enter an option: ")
         if menu_input == '1':
-            password = input("Please enter your password to encode: ")
-            saved_pass = encode(password)
-            # print(saved_pass)
+            saved_pass = encode(input("Please enter your password to encode: "))
+            print("Your password has been encoded and stored!")
         elif menu_input == '2':
             if saved_pass != '':
-                print(saved_pass)
+                print(f"The encoded password is {saved_pass}, and the original password is {decode(saved_pass)}.")
             else:
-                continue
+                print("No Password Input to Decode.")
         elif menu_input == '3':
             exit(0)
         else:
